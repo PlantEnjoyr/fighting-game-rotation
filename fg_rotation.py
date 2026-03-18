e1574ad6cb484ec4180986fdf0206cfc5a30af55
Plant Enjoyer <abimrah1234@gmail.com>
2026-03-17 21:08:04 -0400

    Made new functionality of adding players to the rotation

--- fg_rotation.py
+++ fg_rotation.py
@@ -1,11 +1,13 @@
+#Functions
+
 def insert_name(default_name, list):
-
     ans = input("Enter name of " + default_name + ": ")
-
     if ans == "":
         list.append(default_name)
     else:
         list.append(ans)
+
+
 
 name = []
 rotation = ["no rotation selected :P"]
@@ -62,6 +64,97 @@ else:
     print("erm no :3c")
     action = "q"
 
+#Another Function
+def insert_player(r_number, i):
+    
+    if r_number == 3:
+        insert_name("P4", name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[0], name[0] + " vs " + name[2], name[1] + " vs " + name[3]]
+        
+        if i == 2: #All i values are always round number - 1
+            i = 4 
+
+        r_number = "4"
+    
+    elif r_number == 4:
+        insert_name("P5", name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[4], name[4] + " vs " + name[0], name[0] + " vs " + name[2], name[2] + " vs " + name[4], name[4] + " vs " + name[1], name[1] + " vs " + name[3], name[3] + " vs " + name[0]]
+    
+        if i == 3:
+            i = 9
+        elif i == 4:
+            i = 5
+        elif i == 5:
+            i = 8
+
+        r_number = "5"
+
+    elif r_number == 5:
+        insert_name("P6", name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[4], name[4] + " vs " + name[5], name[5] + " vs " + name[0], name[0] + " vs " + name[2], name[2] + " vs " + name[4], name[4] + " vs " + name[0], name[1] + " vs " + name[3], name[3] + " vs " + name[5], name[5] + " vs " + name[1], name[0] + " vs " + name[3], name[1] + " vs " + name[4], name[2] + " vs " + name[5]]
+    
+        if i == 4:
+            i = 8
+        elif i == 5 or i == 6 or i == 8:
+            i += 1
+        elif i == 7:
+            i = 13
+        elif i == 9:
+            i = 12
+
+        r_number = "6"
+
+    elif r_number == 6:
+        insert_name("P7", name) 
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[4], name[4] + " vs " + name[5], name[5] + " vs " + name[6], name[6] + " vs " + name[0], name[0] + " vs " + name[2], name[2] + " vs " + name[4], name[4] + " vs " + name[6], name[6] + " vs " + name[1], name[1] + " vs " + name[3], name[3] + " vs " + name[5], name[5] + " vs " + name[0], name[0] + " vs " + name[3], name[3] + " vs " + name[6], name[6] + " vs " + name[2], name[2] + " vs " + name[5], name[5] + " vs " + name[1], name[1] + " vs " + name[4], name[4] + " vs " + name[0]]   
+    
+        if i == 5:
+            i = 13
+        elif i == 6 or i == 7:
+            i += 1
+        elif i == 8:
+            i = 20
+        elif i == 9 or i == 10 or i == 12:
+            i += 2
+        elif i == 11:
+            i = 18
+        elif i == 13:
+            i = 19
+        elif i == 14:
+            i = 17
+
+        r_number = "7"
+
+    elif r_number == 7:
+        insert_name("P8", name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[4], name[4] + " vs " + name[5], name[5] + " vs " + name[6], name[6] + " vs " + name[7], name[7] + " vs " + name[0], name[0] + " vs " + name[2], name[2] + " vs " + name[4], name[4] + " vs " + name[6], name[6] + " vs " + name[0], name[1] + " vs " + name[3], name[3] + " vs " + name[5], name[5] + " vs " + name[7], name[7] + " vs " + name[1], name[0] + " vs " + name[3], name[3] + " vs " + name[6], name[6] + " vs " + name[1], name[1] + " vs " + name[4], name[4] + " vs " + name[7], name[7] + " vs " + name[2], name[2] + " vs " + name[5], name[5] + " vs " + name[0], name[0] + " vs " + name[4], name[1] + " vs " + name[5], name[2] + " vs " + name[6], name[3] + " vs " + name[7]]
+    
+        if i == 6:
+            i = 11
+        elif i == 7 or i == 8 or i == 9 or i == 11 or i == 12:
+            i += 1
+        elif i == 10:
+            i = 18
+        elif i == 13:
+            i = 23
+        elif i == 14 or i == 15:
+            i += 2
+        elif i == 16:
+            i = 26
+        elif i == 17:
+            i = 22
+        elif i == 18:
+            i = 25
+        elif i == 20:
+            i = 24
+        
+        r_number = "8"
+    
+    elif r_number == 8:
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[4], name[4] + " vs " + name[5], name[5] + " vs " + name[6], name[6] + " vs " + name[7], name[7] + " vs " + name[0], name[0] + " vs " + name[2], name[2] + " vs " + name[4], name[4] + " vs " + name[6], name[6] + " vs " + name[0], name[1] + " vs " + name[3], name[3] + " vs " + name[5], name[5] + " vs " + name[7], name[7] + " vs " + name[1], name[0] + " vs " + name[3], name[3] + " vs " + name[6], name[6] + " vs " + name[1], name[1] + " vs " + name[4], name[4] + " vs " + name[7], name[7] + " vs " + name[2], name[2] + " vs " + name[5], name[5] + " vs " + name[0], name[0] + " vs " + name[4], name[1] + " vs " + name[5], name[2] + " vs " + name[6], name[3] + " vs " + name[7]]
+        print("Cannot add more players!!")
+
+    return rotation, i, r_number
 
 # The Actual Rotation Function
 i = int(0)
@@ -76,6 +169,10 @@ while action != "q":
         print("\nRound " + str(i+1) + ": " + rotation[i] + "\n")
     elif action == "q":
         print("\nThanks for using this tool!")
+    elif action == "a":
+        print("\n")
+        rotation, i, r_number = insert_player(int(r_number), i)
+        print("\nRound " + str(i+1) + ": " + rotation[i] + "\n")
     else:
         print("\nRound " + str(i+1) + ": " + rotation[i] + "\n")
     
