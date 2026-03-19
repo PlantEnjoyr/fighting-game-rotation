220f847778ef09b527159446abbf7f11e010a19a
Plant Enjoyer <abimrah1234@gmail.com>
2026-03-19 10:44:07 -0400

    Added Player Removal Function

--- fg_rotation.py
+++ fg_rotation.py
@@ -1,5 +1,13 @@
-#Functions
+"""
+-------------------------------------------------------
+Code by PlantEnjoyer
+GitHub: https://github.com/PlantEnjoyr
+Email: abimrah1234@gmail.com
+-------------------------------------------------------
+Theory/Abstraction: https://plant-enjoyer.neocities.org/fighting-game-rotations
+"""
 
+#Functions
 def insert_name(default_name, list):
     ans = input("Enter name of " + default_name + ": ")
     if ans == "":
@@ -7,7 +15,11 @@ def insert_name(default_name, list):
     else:
         list.append(ans)
 
-
+def remove_name(list, key):
+    for i in list:
+        if i == key:
+            list.remove(key)
+    return
 
 name = []
 rotation = ["no rotation selected :P"]
@@ -157,6 +169,123 @@ def insert_player(r_number, i):
 
     return rotation, i, r_number
 
+def remove_player(r_number, i):
+
+    if r_number != 3:
+        rm_name = input("Input player name to remove:\n")
+
+    if r_number == 3:
+        print("Can't remove any more players!!")
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[0]]
+        r_number = "3"
+    
+    elif r_number == 4:
+        remove_name(name, rm_name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[0]]
+        r_number = "3"
+
+        if i == 5:
+            i = 0
+        elif i == 3 or i == 4:
+            i = 2
+    
+    elif r_number == 5:
+        remove_name(name, rm_name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[0], name[0] + " vs " + name[2], name[1] + " vs " + name[3]]
+        r_number = "4"
+
+        if i == 3 or i == 5:
+            i = 4
+        elif i == 6 or i == 7 or i == 8:
+            i = 5
+        elif i == 9:
+            i = 3
+    
+    elif r_number == 6:
+        remove_name(name, rm_name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[4], name[4] + " vs " + name[0], name[0] + " vs " + name[2], name[2] + " vs " + name[4], name[4] + " vs " + name[1], name[1] + " vs " + name[3], name[3] + " vs " + name[0]]
+        r_number = "5"
+
+        if i == 4 or i == 5 or i == 6:
+            i = 5
+        elif i == 7:
+            i = 6
+        elif i == 8:
+            i = 4
+        elif i == 9:
+            i = 8
+        elif i == 10 or i == 11 or i == 12:
+            i = 9
+        elif i == 13:
+            i = 7
+        elif i == 14:
+            i = 0
+        
+    elif r_number == 7:
+        remove_name(name, rm_name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[4], name[4] + " vs " + name[5], name[5] + " vs " + name[0], name[0] + " vs " + name[2], name[2] + " vs " + name[4], name[4] + " vs " + name[0], name[1] + " vs " + name[3], name[3] + " vs " + name[5], name[5] + " vs " + name[1], name[0] + " vs " + name[3], name[1] + " vs " + name[4], name[2] + " vs " + name[5]]
+        r_number = "6"
+
+        if i == 5 or i == 6 or i == 7:
+            i = 6
+        elif i == 8:
+            i = 7
+        elif i == 10 or i == 11:
+            i = 9
+        elif i == 12:
+            i = 10
+        elif i == 13:
+            i = 5
+        elif i == 14:
+            i = 12
+        elif i == 15 or i == 16 or i == 17:
+            i = 14
+        elif i == 18:
+            i = 11
+        elif i == 19:
+            i = 13
+        elif i == 20:
+            i = 8
+    
+    elif r_number == 8:
+        remove_name(name, rm_name)
+        rotation = [name[0] + " vs " + name[1], name[1] + " vs " + name[2], name[2] + " vs " + name[3], name[3] + " vs " + name[4], name[4] + " vs " + name[5], name[5] + " vs " + name[6], name[6] + " vs " + name[0], name[0] + " vs " + name[2], name[2] + " vs " + name[4], name[4] + " vs " + name[6], name[6] + " vs " + name[1], name[1] + " vs " + name[3], name[3] + " vs " + name[5], name[5] + " vs " + name[0], name[0] + " vs " + name[3], name[3] + " vs " + name[6], name[6] + " vs " + name[2], name[2] + " vs " + name[5], name[5] + " vs " + name[1], name[1] + " vs " + name[4], name[4] + " vs " + name[0]]   
+        r_number = "7"
+
+        if i == 6 or i == 8:
+            i = 7
+        elif i == 9:
+            i = 8
+        elif i == 10:
+            i = 9
+        elif i == 11:
+            i = 6
+        elif i == 12:
+            i = 11
+        elif i == 13:
+            i = 12
+        elif i == 15 or i == 16:
+            i = 14
+        elif i == 17:
+            i = 15
+        elif i == 18:
+            i = 10
+        elif i == 20 or i == 21 or i == 22:
+            i = 17
+        elif i == 23:
+            i = 13
+        elif i == 24:
+            i = 20
+        elif i == 25:
+            i = 18
+        elif i == 26:
+            i = 16
+        elif i == 27:
+            i = 1
+
+    return rotation, i, r_number
+
+
 # The Actual Rotation Function
 i = int(0)
 print("\nRound " + str(i+1) + ": " + rotation[i] + "\n")
@@ -173,6 +302,10 @@ while action != "q":
     elif action == "a":
         print("\n")
         rotation, i, r_number = insert_player(int(r_number), i)
+        print("\nRound " + str(i+1) + ": " + rotation[i] + "\n")
+    elif action == "r":
+        print("\n")
+        rotation, i, r_number = remove_player(int(r_number), i)
         print("\nRound " + str(i+1) + ": " + rotation[i] + "\n")
     else:
         print("\nRound " + str(i+1) + ": " + rotation[i] + "\n")
